6d324957169ba54315e6d64808deee239c2e5da4
akita <klepanosovnl@gmail.com>
2020-07-09 11:53:41 +0300

    state for react component

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -2,15 +2,35 @@ import React, {Component} from "react";
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
+    onLabelClick = () => {
+        this.setState({
+            done: true
+        })
+    }
+
+    state = {
+        done: false
+    }
+
     render() {
         const {label, important = false} = this.props;
-
+        const {done} = this.state
         const style = {
-            color: important ? 'tomato' : 'black'
+            color: important ? 'steelblue' : 'black'
         }
+
+        let classNames = "todo-list-item";
+        if (done) {
+            classNames += " done";
+        }
+
         return (
-            <div className="todo-list-item">
-                <span className="todo-list-item-label" style={style}>{label}</span>
+            <div className={classNames}>
+                <span
+                    className="todo-list-item-label"
+                    style={style}
+                    onClick={this.onLabelClick}
+                >{label}</span>
                 <button className="btn btn-outline-danger btn-sm float-right">
                     <i className="fa fa-trash-o"></i>
                 </button>
