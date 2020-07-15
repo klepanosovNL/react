36a975b1f280ac97852b0bba6e45fc0b04512c21
akita <klepanosovnl@gmail.com>
2020-07-15 16:10:38 +0300

    added react components and css

---
+++ star-db/src/components/planet-details/index.js
@@ -0,0 +1,3 @@
+import PlanetDedtails from "./planet-details";
+
+export default PlanetDedtails
---
+++ star-db/src/components/planet-details/planet-details.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./planet.details.css"
+
+const PlanetDedtails = () => {
+
+}
+
+export default PlanetDedtails
