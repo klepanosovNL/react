4fa1bf141c553405240b285614ccdb31335337f2
akita <klepanosovnl@gmail.com>
2020-07-24 14:55:27 +0300

    added loading

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 import "./random-planet.css"
 import SwapiService from "../../sevices/swapi-services";
+import Spinner from "../spinner";
 
 export default class RandomPlanet extends Component {
     state = {
-        planet: {}
+        planet: {},
+        loading: true
     }
 
     swapiService = new SwapiService()
@@ -15,9 +17,11 @@ export default class RandomPlanet extends Component {
     }
 
     onPlanetLoaded = (planet) => {
-        this.setState({planet})
+        this.setState({
+            planet,
+            loading: false
+        })
     }
-
 
     updatedPlanet() {
         const id = Math.floor(Math.random() * 25) + 2
@@ -27,30 +31,43 @@ export default class RandomPlanet extends Component {
     }
 
     render() {
-        const {planet: {population, rotationPeriod, diameter, name, id}} = this.state
+        const {planet, loading} = this.state;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !loading ? <PlanetView planet={planet} /> : null;
 
         return (
             <div className="random-planet jumbotron rounded">
-                <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
-                <div>
-                    <h4>{name}</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{population}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{rotationPeriod}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{diameter}</span>
-                        </li>
-                    </ul>
-                </div>
+                {spinner}
+                {content}
             </div>
         )
     }
+}
+
+const PlanetView = ({planet}) => {
+    const {population, rotationPeriod, diameter, name, id} = planet;
+
+    return (
+        <React.Fragment>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
+            <div>
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
 }
---
+++ star-db/src/components/spinner/spinner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="container">
+            <div className="loadingio-spinner-double-ring-iemki0zgz1">
+                <div className="ldio-cv75npl7vfo">
+                    <div></div>
+                    <div></div>
+                    <div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
